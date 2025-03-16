a4018284d05a538ab89a3f4ab26c18038bf7bf8d
Alvin Chandra Hadiyanto <alvinchandra.h@gmail.com>
2025-03-16 21:44:59 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# This is automation test with cypress
+to install cypress '''npm install cypress'''
+to run cypress '''npx cypress open'''
+
+# I tested webdemosqa web
+https://github.com/UpilBebek/webdemosqa
+
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,18 @@
+describe('webdemosqa', () => {
+  it('Testing webdemosqa can be opened', () => {
+    cy.visit('http://127.0.0.1:8000/')
+      .get('h1')
+      .should('have.text', 'The install worked successfully! Congratulations!');
+  });
+  it('Testing webdemosqa login', () => {
+    cy.login('admin', 'admin'); // Use the custom command
+    cy.get('h1').should('have.text', 'Welcome to Demo SQA Testing Portal');
+
+    // Or, assert that the URL changes to a logged-in page
+    // cy.url().should('include', '/dashboard');
+  });
+  it('Add users webdemosqa', () => {
+    cy.login('admin', 'admin'); // Use the custom command for login
+    cy.get('.model-user > :nth-child(2) > .addlink').click();
+  });
+});
